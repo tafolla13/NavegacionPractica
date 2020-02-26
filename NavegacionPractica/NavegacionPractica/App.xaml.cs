ed2ecd542cf1cd81866af6f122eb447ecc2f0a9a
Lab5 <Lab5@DESKTOP-GFPEOE1>
2020-02-26 15:39:39 -0600

    xD

---
+++ NavegacionPractica/NavegacionPractica/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavegacionPractica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
